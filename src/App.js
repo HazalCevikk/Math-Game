282ca691f0d41a4426c79b3d17667eca3706c4d3
HazalCevikk <hazal.cevikk@gmail.com>
2022-07-16 14:34:24 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Start from "./pages/start";
+import Exam from "./pages/exam";
+import Result from "./pages/result";
+
+import { ScoreProvider } from "./Context/Context";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ScoreProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/Exam" element={<Exam />} />
+          <Route path="/Result" element={<Result />} />
+        </Routes>
+      </BrowserRouter>
+    </ScoreProvider>
   );
 }
 
